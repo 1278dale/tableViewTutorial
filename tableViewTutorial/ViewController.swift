05df22b6e745edbedb584eab45779862185dcd0c
Dale <dalestevens1278@gmail.com>
2015-04-07 15:39:49 +0100

    Thread 1 Sigabrt error, - NEED TO FIX

--- tableViewTutorial/ViewController.swift
+++ tableViewTutorial/ViewController.swift
@@ -8,24 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableView {
     
     let tableData = ["One", "Two", "Three"]
     // swift has inferred the constant as an array of strings
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     func tableView(tableView: UITableView!, numberofRowsInSection section: Int) -> Int
     { return self.tableData.count;
     }
-
+// Table view will have 3 rows according to the count method of the Array class - class method called using the . syntax
+    
+    func tableView(tableView: UITableView!,
+        cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
+    {
+        let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        cell.textLabel!.text = tableData[indexPath.row]
+        
+        return cell
+        
+    
+    }
 }
 
